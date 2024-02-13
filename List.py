509c448dc073c173451da99c6078aba3487d786b
varshini p <60183760+varshini1605@users.noreply.github.com>
2024-02-13 16:32:10 +0000

    LIST AND DICT

--- List.py
+++ List.py
@@ -1,2 +1,9 @@
 #Lists need not be homogeneous always which makes it the most powerful tool in Python
 #Python len() is used to get the length of the list.
+X= ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+Y = [0,   1,   1,    0,   1,   2,   2,   0,   1]
+l={}
+final_list={}
+l={X[i]:Y[i] for i in range(len(Y))}
+#d={k:v for k, v in sorted(l)}
+print(l)
